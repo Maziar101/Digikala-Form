9d0d04ee065b2c6b98a089ceb2a2313d977f093e
maziar <maziarzamani85@gmail.com>
2023-07-30 13:27:14 +0330

    Upload Digikala Signin Form on Github

---
+++ Js/script.js
@@ -0,0 +1,5 @@
+let fr = document.getElementById("form");
+fr.addEventListener("click" , function(){
+    document.getElementById("spc-text").style.display = "block";
+    document.getElementById("dg-input").style.border = "1px solid #EF4056"
+});
